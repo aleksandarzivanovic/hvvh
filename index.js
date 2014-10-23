bcf554d086b15ec9e10d980fd8ecfc65adf9cd21
Aleksandar Zivanovic <aleksandar.zivanovic@shiftplanning.com>
2014-10-23 16:50:12 +0200

    update

--- index.js
+++ index.js
@@ -171,8 +171,8 @@ app.use(compression());
 app.use(multipart());
 app.use(cookieParser());
 app.use(htaccess);
-app.use(express.static(docroot, {expires: (Date.now()*1000) + 86400000}));
 app.use(cachectrl);
 app.use(phpcgi);
+app.use(express.static(docroot, {'Expires': (Date.now()*1000) + 86400000}));
 
 server.listen(argv.port || 8080);
