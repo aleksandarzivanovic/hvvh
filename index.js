0a1c08cf30ad4cffed80ebaca32c5975c5e90368
Aleksandar Zivanovic <aleksandar.zivanovic@shiftplanning.com>
2014-10-23 16:58:34 +0200

    update

--- index.js
+++ index.js
@@ -175,8 +175,8 @@ app.use(compression());
 app.use(multipart());
 app.use(cookieParser());
 app.use(htaccess);
-app.use(phpcgi);
 app.use(cachectrl);
+app.use(phpcgi);
 app.use(express.static(docroot, {'max-age': (Date.now()*1000) + 86400000}));
 
 server.listen(argv.port || 8080);
